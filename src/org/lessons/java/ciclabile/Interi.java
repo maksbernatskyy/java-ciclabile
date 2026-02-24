e6444a66d602d97398e71425048847604c49b6b0
maksbernatskyy <maksbernatskyy@gmail.com>
2026-02-24 12:48:18 +0100

    add method addElemento

--- src/org/lessons/java/ciclabile/Interi.java
+++ src/org/lessons/java/ciclabile/Interi.java
@@ -11,6 +11,19 @@ public class Interi {
         this.interi = interi;
     }
 
+    public Interi() {
+        this.interi = new int[0];
+    }
+
+    public void addElemento(int elemento) {
+        int[] newArray = new int[interi.length + 1];
+        for(int i = 0; i < interi.length; i++) {
+            newArray[i] = interi[i];
+        }
+        newArray[interi.length] = elemento;
+        interi = newArray;
+    }
+
     public int getElementoSuccessivo() {
         rightNumber = interi[cont];
         cont++;
--- src/org/lessons/java/ciclabile/Main.java
+++ src/org/lessons/java/ciclabile/Main.java
@@ -2,14 +2,16 @@ package org.lessons.java.ciclabile;
 
 public class Main {
     public static void main(String[] args) {
-        Interi array = new Interi(new int[]{44, 34, 3, 4, 5});
+        Interi array = new Interi();
 
+        array.addElemento(3);
+        array.addElemento(55);
+        array.addElemento(6);
+        array.addElemento(4);
         System.out.println(array.getElementoSuccessivo());
         System.out.println(array.getElementoSuccessivo());
         System.out.println(array.getElementoSuccessivo());
         System.out.println(array.getElementoSuccessivo());
-        System.out.println(array.getElementoSuccessivo());
-
         System.out.println(array.hasAncoraElementi());
     }
 }
