26fd56207502d64c051a4e8af23c71fe8accb33f
maksbernatskyy <maksbernatskyy@gmail.com>
2026-02-24 12:22:45 +0100

    add method getElementoSuccessivo

--- src/org/lessons/java/ciclabile/Interi.java
+++ src/org/lessons/java/ciclabile/Interi.java
@@ -1,5 +1,13 @@
 package org.lessons.java.ciclabile;
 
 public class Interi {
-
+    private int[] interi = {6, 2, 3, 4, 5};
+    private int cont ;
+    private int rightNumber;
+    // Metodi
+    public int getElementoSuccessivo() {
+        rightNumber = interi[cont];
+        cont++;
+        return rightNumber;
+    }
 }
--- src/org/lessons/java/ciclabile/Main.java
+++ src/org/lessons/java/ciclabile/Main.java
@@ -1,5 +1,13 @@
 package org.lessons.java.ciclabile;
 
 public class Main {
+    public static void main(String[] args) {
+        Interi array = new Interi();
 
+        System.out.println(array.getElementoSuccessivo());
+        System.out.println(array.getElementoSuccessivo());
+        System.out.println(array.getElementoSuccessivo());
+        System.out.println(array.getElementoSuccessivo());
+        System.out.println(array.getElementoSuccessivo());
+    }
 }
