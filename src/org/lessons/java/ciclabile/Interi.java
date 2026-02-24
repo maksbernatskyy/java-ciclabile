7eeb27598ccf7d60f0c65ffc99a54fbdae7f16b8
maksbernatskyy <maksbernatskyy@gmail.com>
2026-02-24 12:34:45 +0100

    add constructor

--- src/org/lessons/java/ciclabile/Interi.java
+++ src/org/lessons/java/ciclabile/Interi.java
@@ -1,11 +1,16 @@
 package org.lessons.java.ciclabile;
 
 public class Interi {
-    private int[] interi = {6, 2, 3, 4, 5};
-    private int cont ;
+    private int[] interi;
+    private int cont;
     private int rightNumber;
     private boolean ancoraElementi = true;
+
     // Metodi
+    public Interi(int[] interi) {
+        this.interi = interi;
+    }
+
     public int getElementoSuccessivo() {
         rightNumber = interi[cont];
         cont++;
--- src/org/lessons/java/ciclabile/Main.java
+++ src/org/lessons/java/ciclabile/Main.java
@@ -2,7 +2,7 @@ package org.lessons.java.ciclabile;
 
 public class Main {
     public static void main(String[] args) {
-        Interi array = new Interi();
+        Interi array = new Interi(new int[]{44, 34, 3, 4, 5});
 
         System.out.println(array.getElementoSuccessivo());
         System.out.println(array.getElementoSuccessivo());
